4b962f60065df0ec490ae5effb35c111ed893d9c
Aqshola <aqsholafid2@gmail.com>
2021-12-04 11:03:42 +0700

    change library

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const video = document.getElementById("video");
+getResult();
 
 function init() {
   document.querySelector(".content").scrollIntoView();
@@ -16,10 +17,7 @@ function init() {
       const detections = await faceapi
         .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
         .withFaceExpressions();
-
-      const resizedDetections = faceapi.resizeResults(detections, displaySize);
-      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-      faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+      getResult(detections);
     }, 500);
   });
 }
@@ -35,6 +33,36 @@ function startVideo() {
 
 const startLoading = () => {
   document.querySelector(".loading").style.display = "flex";
+};
+
+function getResult(result = []) {
+  const detailContainer = document.getElementById("detail-container");
+  if (result.length > 0) {
+    const dataDetect = result[0].expressions;
+    const sorted = [];
+
+    for (const data in dataDetect) {
+      sorted.push({
+        label: data,
+        value: dataDetect[data],
+      });
+    }
+
+    sorted.sort((a, b) => b.value - a.value);
+
+    document.querySelector(".hasil-result").innerHTML = sorted[0].label;
+    detailContainer.innerHTML = appendDetail(sorted);
+  }
+}
+
+const appendDetail = (arr = []) => {
+  let detail = ``;
+  for (let index = 0; index < arr.length - 1; index++) {
+    detail += `${arr[index].label} ( ${Number(arr[index].value).toFixed(
+      2
+    )} ) <br>`;
+  }
+  return detail;
 };
 
 const displayTool = () => {
